de562fe8a3971487e1a9fa7a3568701bb0578421
Angela Wolff <warriorwomenblog@gmail.com>
2018-11-27 18:23:18 +0000

    Correcting placement of simplecov in helper and installing missing dependencies

--- Ruby/ruby_rspec/spec/spec_helper.rb
+++ Ruby/ruby_rspec/spec/spec_helper.rb
@@ -1,10 +1,11 @@
 # Bring in the contents of the `app.rb` file
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
-
-require 'capybara'
-require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+SimpleCov.start
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'capybara'
+require 'capybara/rspec'
+
 
 Capybara.app = MyRackApp
 
@@ -13,4 +14,4 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   # Want a nice code coverage website? Uncomment this next line!
   SimpleCov::Formatter::HTMLFormatter
 ])
-SimpleCov.start
+
